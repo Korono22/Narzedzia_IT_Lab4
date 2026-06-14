9d69f5eb3a7ee750b98afe50eadc75885796baae
Antoni <50383038+Korono22@users.noreply.github.com>
2026-06-14 12:37:41 +0200

    Dodano funkcjonalnosc --logs na 100 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ] && [ -z "$2" ]; then
+    for i in {1..100}
+    do
+        filename="log${i}.txt"
+        echo -e "Nazwa pliku: $filename\nNazwa skryptu: $0\nData: $(date)" > $filename
+    done
 fi
