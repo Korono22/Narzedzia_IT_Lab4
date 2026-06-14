714f1b21c60817be28a4241f5a5ffdee41aacc13
Antoni <50383038+Korono22@users.noreply.github.com>
2026-06-14 12:38:19 +0200

    Dodano obsluge dynamicznej liczby plikow log

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
     date
-elif [ "$1" == "--logs" ] && [ -z "$2" ]; then
-    for i in {1..100}
+elif [ "$1" == "--logs" ]; then
+    count=100
+    if [ -n "$2" ] && [[ "$2" =~ ^[0-9]+$ ]]; then
+        count=$2
+    fi
+
+    for ((i=1; i<=count; i++))
     do
         filename="log${i}.txt"
         echo -e "Nazwa pliku: $filename\nNazwa skryptu: $0\nData: $(date)" > $filename
